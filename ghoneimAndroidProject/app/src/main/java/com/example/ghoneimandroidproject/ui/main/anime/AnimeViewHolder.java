b3eca46ddbc0a7bba907f034bac69bce7a54fc2c
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-01-24 22:53:50 +0100

    FEAT: init the anime view holder

---
+++ ghoneimAndroidProject/app/src/main/java/com/example/ghoneimandroidproject/ui/main/anime/AnimeViewHolder.java
@@ -0,0 +1,16 @@
+package com.example.ghoneimandroidproject.ui.main.anime;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AnimeViewHolder extends RecyclerView.ViewHolder{
+
+    private View itemView;
+
+    public AnimeViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.itemView = itemView;
+    }
+}
